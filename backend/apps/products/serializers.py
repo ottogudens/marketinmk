9a61437abfad085218a7db2339a6e0cdf3627980
ottogudens <ottogudenschwager@gmail.com>
2026-08-09 21:19:41 -0400

    fix: import Coupon and Payment models in products/serializers.py

--- backend/apps/products/serializers.py
+++ backend/apps/products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Product, Offer, OfferRedemption, OfferView
+from .models import Category, Product, Offer, OfferRedemption, OfferView, Coupon, Payment
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
